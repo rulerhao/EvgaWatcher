ee06ab07222044390163a4784cdf3b3fa93f0c09
rulerhao <z19970615@gmail.com>
2022-05-28 13:54:44 +0800

    1. Finished crawler.

--- app/src/main/java/com/rulhouse/evgawatcher/WebCrawler.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/WebCrawler.kt
@@ -15,28 +15,20 @@ class WebCrawler {
     suspend fun test() {
         withContext(Dispatchers.IO) {
             try {
-                Log.d("TestText", "start")
                 val downloadUrl = "http://tw.evga.com/products/productlist.aspx?type=0"
 
                 val itemsName = mutableListOf<String>()
+                val itemUrl = mutableListOf<String>()
                 val itemsImgUrl = mutableListOf<String>()
                 val itemCanBuy = mutableListOf<Boolean>()
-                val serial = mutableListOf<String>()
+                val itemSerial = mutableListOf<String>()
                 val itemDetails = mutableListOf<List<String>>()
+                val itemLimit = mutableListOf<String>()
+                val itemPrice = mutableListOf<String>()
 
                 val doc: Document = Jsoup.connect(downloadUrl)
-//                    .userAgent("Mozilla")
-//                    .userAgent("Chrome")
-//                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
-//                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36")
                     .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582")
-//                    .header("key", "value")
-//                    .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-//                    .data("key", "value")
-//                    .referrer("http://www.google.com")
-//                    .header("Accept", "text/html")
                     .header("Connection", "keep-alive")
-//                    .ignoreContentType(true)
                     .get()
 
                 val gridItemElements = doc.getElementsByClass("grid-item")
@@ -44,13 +36,20 @@ class WebCrawler {
                     val plGridImageElements = element.getElementsByClass("pl-grid-image")
                     for (plGridImageElement in plGridImageElements) {
                         val item = plGridImageElement.child(1)
+                        // Url
+                        itemUrl.add(item.attr("href"))
+                        // Title
                         itemsName.add(item.attr("title"))
+                        // Image Url
                         itemsImgUrl.add(item.getElementsByTag("img").attr("src"))
-                        serial.add(item.getElementsByTag("img").attr("alt"))
+                        // Serial
+                        itemSerial.add(item.getElementsByTag("img").attr("alt"))
                     }
+                    // Item can buy
                     val btnAddCartElements = element.getElementsByClass("btnAddCart")
                     itemCanBuy.add(!btnAddCartElements.isEmpty())
 
+                    // Item details
                     val plGridInfo = element.getElementsByClass("pl-grid-info")
                     plGridInfo.forEach { element ->
                         val ulList = element.getElementsByTag("ul")
@@ -61,15 +60,26 @@ class WebCrawler {
                         }
                         itemDetails.add(detailsList)
                     }
-                }
-                for (gridItemElement in gridItemElements) {
+                    // Limit
+                    val productLimit = element.getElementsByClass("limit").text()
+                    itemLimit.add(productLimit)
 
+                    // Price
+                    val priceClass = element.getElementsByClass("pl-grid-price")
+                    priceClass.forEach { element ->
+                        val price = element.getElementsByTag("strong").text()
+                        itemPrice.add(price)
+                    }
                 }
                 Log.d("TestText", "item.title = $itemsName")
                 Log.d("TestText", "item.img = $itemsImgUrl")
-                Log.d("TestText", "item.serial = $serial")
+                Log.d("TestText", "item.serial = $itemSerial")
                 Log.d("TestText", "item.itemCanBuy = $itemCanBuy")
                 Log.d("TestText", "item.itemDetails = $itemDetails")
+                Log.d("TestText", "item.itemUrl = $itemUrl")
+                Log.d("TestText", "item.itemLimit = $itemLimit")
+                Log.d("TestText", "item.itemPrice = $itemPrice")
+
             } catch (e: Exception) {
                 Log.d("TestText", "e = $e")
                 e.printStackTrace()
