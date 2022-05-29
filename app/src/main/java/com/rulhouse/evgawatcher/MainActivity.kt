433875b17cc2d125b4c5213290ddd6f18f2adcc0
rulerhao <z19970615@gmail.com>
2022-05-29 13:44:56 +0800

    1. Added gpu items in main activity.

--- app/src/main/java/com/rulhouse/evgawatcher/MainActivity.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.rulhouse.evgawatcher
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import com.rulhouse.evgawatcher.crawler.use_cases.CrawlerUseCases
 import com.rulhouse.evgawatcher.presentation.MainScreen
 import com.rulhouse.evgawatcher.ui.theme.EvgaWatcherTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var gpuProductsUseCases: CrawlerUseCases
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,6 +29,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    LaunchedEffect(key1 = true) {
+                        Log.d("gpuProductsUseCases", "gpuProductsUseCases ${gpuProductsUseCases.getGpuItems()}")
+                    }
 //                    this.openUri("https://www.polygreen.com.tw/")
                     MainScreen()
 
