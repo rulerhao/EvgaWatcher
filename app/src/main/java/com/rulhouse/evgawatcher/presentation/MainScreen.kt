dcd7287d6c14d6ee7b18c9a782b2e0e8b85c3762
rulerhao <z19970615@gmail.com>
2022-05-30 01:07:37 +0800

    1. Added regular expression for get the general gpu card name.

--- app/src/main/java/com/rulhouse/evgawatcher/presentation/MainScreen.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/presentation/MainScreen.kt
@@ -1,26 +1,47 @@
 package com.rulhouse.evgawatcher.presentation
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun MainScreen(
     viewModel: MainScreenViewModel = hiltViewModel()
 ) {
-    Text(
-        text = "Test"
-    )
+    val navController = rememberNavController()
+
+//    NavHost(
+//        navController = navController,
+//        startDestination = Screen.AllProductsScreenScreen.route
+//    ) {
+//        composable(route = Screen.AllProductsScreenScreen.route) {
+//            SplashScreen(navController = navController)
+//        }
+//        composable(route = Screen.FavoriteProductsScreen.route) {
+//            UserScreen(navController = navController)
+//        }
+//    }
     LazyColumn(
-        
+        modifier = Modifier
+            .padding(16.dp)
     ) {
         val products = viewModel.products.value
         if (products != null) {
             items(products) { item ->
-                GpuProductItem(item = item)
+                GpuProductItem(
+                    item = item
+                )
             }
         }
     }
