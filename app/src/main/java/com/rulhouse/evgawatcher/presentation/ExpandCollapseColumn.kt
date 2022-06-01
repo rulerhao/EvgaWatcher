025e398fa44bb928bd38e8fb0ce5d17cd68b8328
rulerhao <z19970615@gmail.com>
2022-06-02 00:58:28 +0800

    1. Modified collapsed column layout.

--- app/src/main/java/com/rulhouse/evgawatcher/presentation/ExpandCollapseColumn.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/presentation/ExpandCollapseColumn.kt
@@ -1,19 +1,19 @@
 package com.rulhouse.evgawatcher.presentation
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.rulhouse.evgawatcher.GpuProduct
 
@@ -26,24 +26,32 @@ fun ExpandCollapseColumn(
     Column(
         Modifier
             .fillMaxWidth()
-            .padding(start = 16.dp, top = 4.dp, end = 8.dp, bottom = 4.dp)
+            .padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp)
     ) {
-        Row(
-            Modifier
+        Card(
+            modifier = Modifier
                 .fillMaxWidth()
                 .clickable {
                     onCollapsedStateChanged(!expandCollapseModel.isOpen)
                 },
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = expandCollapseModel.title,
+            Row(
                 modifier = Modifier
-                    .align(Alignment.CenterVertically)
-                    .padding(vertical = 4.dp, horizontal = 8.dp)
-            )
-            ToggleButton(expandCollapseModel)
+                    .fillMaxWidth()
+                    .background(color = MaterialTheme.colors.primary),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = expandCollapseModel.title,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(vertical = 4.dp, horizontal = 8.dp),
+                    color = MaterialTheme.colors.onPrimary,
+                    style = MaterialTheme.typography.h6
+                )
+                ToggleButton(expandCollapseModel)
+            }
         }
         ProductsList(
             expandCollapseModel = expandCollapseModel,
@@ -79,5 +87,6 @@ private fun ToggleButton(
             .size(36.dp),
         imageVector = if (expandCollapseModel.isOpen) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
         contentDescription = null,
+        tint = MaterialTheme.colors.onPrimary
     )
 }
