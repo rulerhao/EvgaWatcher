e839f5d239458bbf79578bd1347adf1606f7303c
rulerhao <z19970615@gmail.com>
2022-06-02 02:52:21 +0800

    1. Added product screenn.

--- app/src/main/java/com/rulhouse/evgawatcher/presentation/Screen.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/presentation/Screen.kt
@@ -3,4 +3,5 @@ package com.rulhouse.evgawatcher.presentation
 sealed class Screen(val route: String) {
     object AllProductsScreenScreen : Screen("all_products_screen")
     object FavoriteProductsScreen : Screen("favorite_products_screen")
+    object ProductScreen : Screen("product_screen")
 }
