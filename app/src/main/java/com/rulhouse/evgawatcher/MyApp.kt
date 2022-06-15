ba250000bf02bd6eeb93c7e37a876a5321482cfe
rulerhao <z19970615@gmail.com>
2022-06-16 01:49:02 +0800

    1. Added crawler work manager test.

--- app/src/main/java/com/rulhouse/evgawatcher/MyApp.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/MyApp.kt
@@ -1,8 +1,19 @@
 package com.rulhouse.evgawatcher
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MyApp : Application() {
+class MyApp: Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
