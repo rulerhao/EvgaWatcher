8794611aa8be5b28a85ca050f3af0a0af97c2ac2
rulerhao <z19970615@gmail.com>
2022-06-21 03:00:59 +0800

    1. Added test difference objects.

---
+++ app/src/test/java/com/rulhouse/evgawatcher/get_different_products/buyable/TestBecomeBuyableObject.kt
@@ -0,0 +1,35 @@
+package com.rulhouse.evgawatcher.get_different_products.buyable
+
+import com.rulhouse.evgawatcher.favorite_products.feature_node.data.GpuProduct
+
+class TestBecomeBuyableObject {
+    val favoriteGpuProduct: List<GpuProduct> = listOf(
+        GpuProduct(
+            name = "EVGA GeForce RTX 3090 Ti FTW3 ULTRA HYBRID GAMING, 24G-P5-4988-KR, 24GB GDDR6X, iCX3, HYBRID Cooler, ARGB LED, Backplate, Free eLeash",
+            imgUrl = "https://images.evga.com/products/gallery/png/24G-P5-4988-KR_MD_1.png",
+            serial = "24G-P5-4988-KR",
+            canBeBought = false,
+            statement = listOf(),
+            url = "https://tw.evga.com/Products/Product.aspx?pn=24G-P5-4988-KR",
+            limitedNumber = "",
+            price = 20000,
+            warranty = "全球保固： 3 年",
+            favorite = true
+        )
+    )
+
+    val crawlerGpuProduct: List<GpuProduct> = listOf(
+        GpuProduct(
+            name = "EVGA GeForce RTX 3090 Ti FTW3 ULTRA HYBRID GAMING, 24G-P5-4988-KR, 24GB GDDR6X, iCX3, HYBRID Cooler, ARGB LED, Backplate, Free eLeash",
+            imgUrl = "https://images.evga.com/products/gallery/png/24G-P5-4988-KR_MD_1.png",
+            serial = "24G-P5-4988-KR",
+            canBeBought = true,
+            statement = listOf(),
+            url = "https://tw.evga.com/Products/Product.aspx?pn=24G-P5-4988-KR",
+            limitedNumber = "",
+            price = 10000,
+            warranty = "全球保固： 3 年",
+            favorite = false
+        )
+    )
+}
