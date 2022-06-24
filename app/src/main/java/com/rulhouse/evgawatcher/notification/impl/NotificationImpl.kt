d9126008c4386a013e2f8e458263851e24a489e4
rulerhao <32173057+rulerhao@users.noreply.github.com>
2022-06-24 13:15:10 +0800

    1. Added reason of notification to notification.

--- app/src/main/java/com/rulhouse/evgawatcher/notification/impl/NotificationImpl.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/notification/impl/NotificationImpl.kt
@@ -1,17 +1,10 @@
 package com.rulhouse.evgawatcher.notification.impl
 
 import android.content.Context
-import android.util.Log
 import com.rulhouse.evgawatcher.data_store.use_cases.NotificationIdDataStoreUseCases
 import com.rulhouse.evgawatcher.notification.repository.NotificationRepository
-import com.rulhouse.evgawatcher.notification.use_case.NotificationUseCase
 import com.rulhouse.evgawatcher.notification_gpu_product_change.ProductsDifference
-import com.rulhouse.evgawatcher.notification_gpu_product_change.impl.NotificationGpuProductChange
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.launchIn
-import javax.inject.Inject
 
 class NotificationImpl (
     private val context: Context,
@@ -23,7 +16,7 @@ class NotificationImpl (
             notificationIdDataStoreUseCases.getNotificationIdDataStoreFlow().collect {
                 val notifyId = it.notificationId
                 differentProducts?.forEach { productDifference ->
-                    DifferentProductsNotification().doNotify(context, notifyId, productDifference)
+                    DifferentProductsNotification().doNotifyProductsDifference(context, notifyId, productDifference)
                     notificationIdDataStoreUseCases.updateNotificationId(notifyId + 1)
                 }
                 this@coroutineScope.cancel()
