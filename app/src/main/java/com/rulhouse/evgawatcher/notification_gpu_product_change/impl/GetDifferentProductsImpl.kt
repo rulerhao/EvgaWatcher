1acbb750eba3efc8da20322edc5f68e6d2fc1f6b
rulerhao <32173057+rulerhao@users.noreply.github.com>
2022-06-24 16:57:22 +0800

    1. Finished update favorite product when click notification button.

--- app/src/main/java/com/rulhouse/evgawatcher/notification_gpu_product_change/impl/GetDifferentProductsImpl.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/notification_gpu_product_change/impl/GetDifferentProductsImpl.kt
@@ -1,14 +1,17 @@
 package com.rulhouse.evgawatcher.notification_gpu_product_change.impl
 
+import com.rulhouse.evgawatcher.crawler.use_cases.CrawlerUseCases
+import com.rulhouse.evgawatcher.favorite_products.feature_node.domain.use_case.FavoriteGpuProductUseCases
 import com.rulhouse.evgawatcher.notification_gpu_product_change.ProductsDifference
 import com.rulhouse.evgawatcher.notification_gpu_product_change.repository.GetDifferentProductsRepository
 import javax.inject.Inject
 
-class GetDifferentProductsImpl: GetDifferentProductsRepository {
-    @Inject
-    lateinit var notificationGpuProductChange: NotificationGpuProductChange
+class GetDifferentProductsImpl(
+    private val crawlerUseCases: CrawlerUseCases,
+    private val favoriteGpuProductUseCases: FavoriteGpuProductUseCases
+): GetDifferentProductsRepository {
 
     override suspend fun getDifference(): List<ProductsDifference> {
-        return notificationGpuProductChange.getDifferenceProducts()
+        return NotificationGpuProductChange(crawlerUseCases, favoriteGpuProductUseCases).getDifferenceProducts()
     }
 }
--- app/src/main/java/com/rulhouse/evgawatcher/work_manager/coroutine_work/CrawlerWorker.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/work_manager/coroutine_work/CrawlerWorker.kt
@@ -6,6 +6,9 @@ import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.rulhouse.evgawatcher.crawler.use_cases.CrawlerUseCases
+import com.rulhouse.evgawatcher.notification.use_case.NotificationUseCase
+import com.rulhouse.evgawatcher.notification_gpu_product_change.ProductsDifference
+import com.rulhouse.evgawatcher.notification_gpu_product_change.use_case.GetDifferentProductsUseCase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 
@@ -13,13 +16,14 @@ import dagger.assisted.AssistedInject
 class CrawlerWorker @AssistedInject constructor(
     @Assisted appContext: Context,
     @Assisted workerParams: WorkerParameters,
-    private val crawlerUseCases: CrawlerUseCases
+    private val getDifferentProductsUseCase: GetDifferentProductsUseCase,
+    private val notificationUseCase: NotificationUseCase
 ): CoroutineWorker(appContext, workerParams){
 
     override suspend fun doWork(): Result {
 
-//        this.applicationContext.
-//        Log.d("TestWorker", "GpuItems = ${crawlerUseCases.getGpuItems()}")
+        val productsDifference = getDifferentProductsUseCase.getDifferenceProducts()
+        notificationUseCase.notificationDifferentProducts(productsDifference)
 
         return Result.success()
     }
