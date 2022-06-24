f49243e9f04ab86d61bb0862150fba5a4cdadabe
rulerhao <32173057+rulerhao@users.noreply.github.com>
2022-06-24 16:35:39 +0800

    1. Added null and empty check.

--- app/src/main/java/com/rulhouse/evgawatcher/notification/impl/NotificationImpl.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/notification/impl/NotificationImpl.kt
@@ -4,7 +4,8 @@ import android.content.Context
 import com.rulhouse.evgawatcher.data_store.use_cases.NotificationIdDataStoreUseCases
 import com.rulhouse.evgawatcher.notification.repository.NotificationRepository
 import com.rulhouse.evgawatcher.notification_gpu_product_change.ProductsDifference
-import kotlinx.coroutines.*
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.coroutineScope
 
 class NotificationImpl (
     private val context: Context,
@@ -12,10 +13,16 @@ class NotificationImpl (
 ) : NotificationRepository {
 
     override suspend fun notificationDifferentProducts(differentProducts: List<ProductsDifference>?) {
+        if (differentProducts == null) {
+            return
+        }
+        if (differentProducts.isEmpty()) {
+            return
+        }
         coroutineScope {
             notificationIdDataStoreUseCases.getNotificationIdDataStoreFlow().collect {
                 val notifyId = it.notificationId
-                differentProducts?.forEach { productDifference ->
+                differentProducts.forEach { productDifference ->
                     DifferentProductsNotification().doNotifyProductsDifference(context, notifyId, productDifference)
                     notificationIdDataStoreUseCases.updateNotificationId(notifyId + 1)
                 }
