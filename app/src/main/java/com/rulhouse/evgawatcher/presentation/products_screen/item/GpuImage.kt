afeb39a348f8a338302fa0de31ea4d77720684bc
rulerhao <32173057+rulerhao@users.noreply.github.com>
2022-06-29 09:56:03 +0800

    1. Optimized constraint for gpu image.

--- app/src/main/java/com/rulhouse/evgawatcher/presentation/products_screen/item/GpuImage.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/presentation/products_screen/item/GpuImage.kt
@@ -1,9 +1,8 @@
 package com.rulhouse.evgawatcher.presentation.products_screen.item
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
@@ -11,17 +10,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import com.rulhouse.evgawatcher.methods.ScreenMethods
+import com.skydoves.landscapist.CircularReveal
 import com.skydoves.landscapist.glide.GlideImage
+import com.skydoves.landscapist.rememberDrawablePainter
 
 @Composable
-fun Image(
-    imgUrl: String?
+fun GpuImage(
+    imgUrl: String?,
+    size: IntSize
 ) {
     GlideImage(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.tertiary)
-            .fillMaxHeight(0.5f),
+            .height(ScreenMethods.convertPixelToDp(size.height.toFloat(), LocalContext.current).dp),
         imageModel = imgUrl,
+        // shows an image with the circular reveal animation.
+        circularReveal = CircularReveal(duration = 2000),
         contentScale = ContentScale.Fit,
         loading = {
             Box {
