0ff9ba11b757f841080b20604d6bdfe897060099
rulerhao <32173057+rulerhao@users.noreply.github.com>
2022-06-29 13:45:57 +0800

    1. Added shop button.

--- app/src/main/java/com/rulhouse/evgawatcher/presentation/products_screen/item/PriceAndStore.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/presentation/products_screen/item/PriceAndStore.kt
@@ -1,16 +1,15 @@
 package com.rulhouse.evgawatcher.presentation.products_screen.item
 
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Storefront
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PriceAndStore (
@@ -44,14 +43,20 @@ private fun Price(price: Int?) {
 private fun Store(buyable: Boolean?) {
     if (buyable != null) {
         val tint = if (buyable) {
-            MaterialTheme.colorScheme.tertiary
+            MaterialTheme.colorScheme.onPrimary
         } else {
-            MaterialTheme.colorScheme.tertiaryContainer
+            MaterialTheme.colorScheme.onPrimaryContainer
         }
-        Icon(
-            imageVector = Icons.Default.Storefront,
-            contentDescription = null,
-            tint = tint
-        )
+        Button(
+            onClick = {
+
+            }
+        ) {
+            Icon(
+                modifier = Modifier,
+                imageVector = Icons.Default.Storefront,
+                contentDescription = null
+            )
+        }
     }
 }
