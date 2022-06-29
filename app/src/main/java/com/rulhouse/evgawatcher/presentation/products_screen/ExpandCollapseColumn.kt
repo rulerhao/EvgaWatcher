c92aec4b937cf373a90dcdac4f2fc31e186a427b
rulerhao <z19970615@gmail.com>
2022-06-30 02:21:37 +0800

    1. Modified products title to Material theme 3.

--- app/src/main/java/com/rulhouse/evgawatcher/presentation/products_screen/ExpandCollapseColumn.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/presentation/products_screen/ExpandCollapseColumn.kt
@@ -1,13 +1,13 @@
 package com.rulhouse.evgawatcher.presentation.products_screen
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,6 +16,7 @@ import androidx.compose.ui.unit.dp
 import com.rulhouse.evgawatcher.favorite_products.feature_node.data.GpuProduct
 import com.rulhouse.evgawatcher.presentation.products_screen.item.ProductItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpandCollapseColumn(
     expandCollapseModel: ExpandCollapseModel,
@@ -38,8 +39,7 @@ fun ExpandCollapseColumn(
         ) {
             Row(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = MaterialTheme.colors.primary),
+                    .fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically,
             ) {
@@ -48,8 +48,6 @@ fun ExpandCollapseColumn(
                     modifier = Modifier
                         .align(Alignment.CenterVertically)
                         .padding(vertical = 4.dp, horizontal = 8.dp),
-                    color = MaterialTheme.colors.onPrimary,
-                    style = MaterialTheme.typography.h6
                 )
                 ToggleButton(expandCollapseModel)
             }
@@ -114,7 +112,6 @@ private fun ToggleButton(
             .padding(8.dp)
             .size(36.dp),
         imageVector = if (expandCollapseModel.isOpen) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
-        contentDescription = null,
-        tint = MaterialTheme.colors.onPrimary
+        contentDescription = null
     )
 }
