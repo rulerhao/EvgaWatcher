e7f2e51840e923247dd9deafd954f631039fb890
rulerhao <32173057+rulerhao@users.noreply.github.com>
2022-06-30 14:22:17 +0800

    1. Hid system bar.

--- app/src/main/java/com/rulhouse/evgawatcher/MainActivity.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rulhouse.evgawatcher
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.rulhouse.evgawatcher.methods.ActivitySetting
 import com.rulhouse.evgawatcher.presentation.products_screen.item.ProductItem
 import com.rulhouse.evgawatcher.presentation.screen.MainScreen
 import com.rulhouse.evgawatcher.ui.theme.AppTheme
@@ -20,8 +21,13 @@ class MainActivity : ComponentActivity() {
 //    @Inject
 //    lateinit var workManagerUseCases: WorkManagerUseCases
 
+    val activitySetting = ActivitySetting()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        activitySetting.hindSystemBar(this)
+
+        activitySetting.addSystemUIListener(this)
 
         setContent {
             AppTheme {
@@ -29,5 +35,11 @@ class MainActivity : ComponentActivity() {
 //                ProductItem()
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        activitySetting.hideSystemBar(this)
     }
 }
---
+++ app/src/main/java/com/rulhouse/evgawatcher/methods/ActivitySetting.kt
@@ -0,0 +1,53 @@
+package com.rulhouse.evgawatcher.methods
+
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import android.view.View
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class ActivitySetting {
+    fun setOrientation(activity: Activity) {
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+    }
+
+    fun setSystemBar(activity: Activity) {
+        WindowCompat.setDecorFitsSystemWindows(activity.window, false)
+        WindowInsetsControllerCompat(activity.window, activity.window.decorView).systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        hideSystemBar(activity)
+    }
+
+    fun hideSystemBar(activity: Activity) {
+        WindowInsetsControllerCompat(activity.window, activity.window.decorView).hide(WindowInsetsCompat.Type.systemBars())
+    }
+
+    private fun setShowSystemUi(show: Boolean, activity: Activity) {
+        WindowCompat.setDecorFitsSystemWindows(activity.window, show)
+        val controller = WindowInsetsControllerCompat(activity.window, activity.window.decorView)
+        if (show) {
+            controller.show(WindowInsetsCompat.Type.systemBars())
+        } else {
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    fun addSystemUIListener (activity: Activity) {
+        activity.window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0)
+                hindSystemBar(activity)
+        }
+    }
+
+    fun hindSystemBar (activity: Activity) = runBlocking {
+        launch {
+            delay(1000L)
+            setShowSystemUi(false, activity)
+        }
+    }
+}
