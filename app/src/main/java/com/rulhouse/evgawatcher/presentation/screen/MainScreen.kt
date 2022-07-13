ba6f159f1984e1ec477a75676c1fb535d3d5199f
rulerhao <z19970615@gmail.com>
2022-07-13 14:20:35 +0800

    1. Deleted unused code.

--- app/src/main/java/com/rulhouse/evgawatcher/presentation/screen/MainScreen.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/presentation/screen/MainScreen.kt
@@ -21,58 +21,46 @@ import com.rulhouse.evgawatcher.presentation.reminde_screen.RemindersScreen
 
 @Composable
 fun MainScreen(
-//    viewModel: CrawlerProductsScreenViewModel = hiltViewModel(),
 ) {
     val viewModel: CrawlerProductsScreenViewModel = hiltViewModel()
 
     val navController = rememberNavController()
 
-//    Scaffold(
-//        modifier = Modifier
-//            .statusBarsPadding()
-//            .navigationBarsPadding(),
-//        bottomBar = {
-//            BottomNavigationBar(navController)
-//        },
-//    ) { innerPadding ->
-//        Box(modifier = Modifier.padding(innerPadding)) {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                NavHost(
-                    navController = navController,
-                    startDestination = Screen.AllProductsScreenScreen.route
-                ) {
-                    composable(route = Screen.AllProductsScreenScreen.route) {
-                        CrawlerProductsScreen(
-                            viewModel,
-                            navController = navController
-                        )
-                    }
-                    composable(route = Screen.FavoriteProductsScreen.route) {
-                        FavoriteProductsScreen(navController = navController)
-                    }
-                    composable(route = Screen.RemindersScreen.route) {
-                        RemindersScreen(navController = navController)
-                    }
-                    composable(
-                        route = Screen.ProductScreen.route + "?" +
-                                "gpuProduct={gpuProduct}",
-                        arguments = listOf(
-                            navArgument(
-                                name = "gpuProduct"
-                            ) {
-
-                                type = GpuProductType()
-
-                            }
-                        )
-                    ) {
-                        ProductScreen(navController = navController)
-                    }
-                }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = Screen.AllProductsScreenScreen.route
+        ) {
+            composable(route = Screen.AllProductsScreenScreen.route) {
+                CrawlerProductsScreen(
+                    viewModel,
+                    navController = navController
+                )
             }
-//        }
-//    }
+            composable(route = Screen.FavoriteProductsScreen.route) {
+                FavoriteProductsScreen(navController = navController)
+            }
+            composable(route = Screen.RemindersScreen.route) {
+                RemindersScreen(navController = navController)
+            }
+            composable(
+                route = Screen.ProductScreen.route + "?" +
+                        "gpuProduct={gpuProduct}",
+                arguments = listOf(
+                    navArgument(
+                        name = "gpuProduct"
+                    ) {
+
+                        type = GpuProductType()
+
+                    }
+                )
+            ) {
+                ProductScreen(navController = navController)
+            }
+        }
+    }
 }
