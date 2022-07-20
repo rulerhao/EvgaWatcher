da9875e0adb8f34ea53da7ecf4b926744030f87a
rulerhao <z19970615@gmail.com>
2022-07-20 19:39:17 +0800

    1. Fixed a bug that splash screen show default icon instead evga watcher icon.

--- app/src/main/java/com/rulhouse/evgawatcher/MainActivity.kt
+++ app/src/main/java/com/rulhouse/evgawatcher/MainActivity.kt
@@ -26,8 +26,6 @@ class MainActivity : ComponentActivity() {
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-//        installSplashScreen()
-
         setContent {
             AppTheme {
                 MainScreen()
